380c75cf6dd76e4d9b18e781b468307a6b111d8e
konqueror7 <konqueror7@gmail.com>
2019-03-08 17:56:48 +0500

    BJS "Wrappers for primitives" commit#3 in master

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -10,36 +10,58 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  /*Создание ассоциативного массива из переменных, полученных функцией в качестве аргументов:
+  Условия кредита creditTerms:
+   percent - процентной ставки,
+   contribution - первоначального взноса,
+   amount - общей суммы кредита
+  Период кредитования - date.
+  */
   let TotalMortgage = {
-    percent: percent,
-    contribution: contribution,
-    amount: amount
+    creditTerms: {
+      percent: percent,
+      contribution: contribution,
+      amount: amount,
+    },
+    date: date
   };
 
-  for (let key in TotalMortgage) {
-    if (parseInt(TotalMortgage[key]) !== parseInt(TotalMortgage[key])) {
-      return totalAmount = `Параметр ${key} содержит неправильное значение ${TotalMortgage[key]}`;
+  //Проверка значений условий кредита creditTerms на соотвествие целочисленному типу даных
+  for (let key in TotalMortgage.creditTerms) {
+    if (parseInt(TotalMortgage.creditTerms[key]) !== parseInt(TotalMortgage.creditTerms[key])) {
+      return totalAmount = `Параметр ${key} содержит неправильное значение ${TotalMortgage.creditTerms[key]}`;
     }
   }
 
+  //Определние текущей даты в стандартном формате
   let currentDate = new Date();
-  let dateRepayment = new Date(date);
+  //Преобразование даты окончания кредитования из строки в экземпляр объекта Date
+  let dateRepayment = new Date(TotalMortgage.date);
+  //Вычисление периода кредитования в месяцах
   let repaymentPeriod = (dateRepayment.getFullYear() - currentDate.getFullYear()) * 12 - (currentDate.getMonth()+1) + (dateRepayment.getMonth()+1);
-  let percentFraction = parseInt(percent) / 100;
-  let downPayment = parseInt(contribution);
-  let loanAmount = parseInt(amount);
+
+  //Преобразование строковых значений усовий кредитования в целочисленные
+  let percentFraction = parseInt(TotalMortgage.creditTerms.percent) / 100;
+  let downPayment = parseInt(TotalMortgage.creditTerms.contribution);
+  let loanAmount = parseInt(TotalMortgage.creditTerms.amount);
+
+  //Вычисление ежемесячной выплаты за кредит
   let monthlyPayment = (loanAmount * (percentFraction + percentFraction / (((1 + percentFraction)**repaymentPeriod) - 1))).toFixed(2);
+
+  //Вычисление общей суммы выплат по кредиту
   totalAmount = (monthlyPayment * repaymentPeriod - downPayment).toFixed(2);
 
+  //Вывод информации о кредите в консоль
   console.log(`
     Текушая дата ${currentDate} \n
     Дата возврата ${dateRepayment} \n
     Период ${repaymentPeriod} \n
-    Процент ${percent} \n
+    Процент ${percentFraction} \n
     Ежемесячный платеж ${monthlyPayment} \n
     Общая сумма ${totalAmount}
     `);
 
+    //Передача результата вычисления переменной totalAmount во внешнее окружение
     return totalAmount;
 }
 
@@ -51,16 +73,21 @@ function sayHello() {
 }
 
 function getGreeting(name) {
+  //Объявление переменной, в которую будет сохраняться имя посетителя
   let userName;
 
+  //Запись в переменную userName значения 'Аноним' если она имеет значения '', 'null', 'undefined'
   if (!name || name.valueOf() == 'null' || name.valueOf() == 'undefined') {
     userName = 'Аноним';
   }
+  // Во всех остальных случаях присвоение введенного значения
   else {
     userName = name;
   }
-
+  //Шаблон вывода приветсвия
   greeting = `Привет, мир! Меня зовут ${userName}.`;
+  //Вывод приветствия в консоль
   console.log(greeting);
+  //передача приветствия во внешнее окружение
   return greeting;
 }
