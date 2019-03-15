abed1baf49b941c98baf0cc94f7cdb9dbecae0a4
konqueror7 <konqueror7@gmail.com>
2019-03-15 09:35:32 +0500

    oop/grades #3

--- oop-es6/main.js
+++ oop-es6/main.js
@@ -11,7 +11,7 @@ class StudentLog {
 
   search(subject) {
     return this.gradesSubjects.find(function(gradesSubject) {
-      return gradesSubject.subject === subject;
+      return gradesSubject === subject;
     });
   }
 
@@ -110,8 +110,8 @@ console.log(log.addGrade('3', 'Ariphmetic'));
 console.log(log.addGrade('4', 'Ariphmetic'));
 // console.log(log.addGrade('24', 'Арифметика'));
 // console.log(log.addGrade('ОТлично!', 'Арифметика'));
-console.log(log.getAverageBySubject('Ariphmetic'));
-console.log(log.getTotalAverage());
+// console.log(log.getAverageBySubject('Ariphmetic'));
+// console.log(log.getTotalAverage());
 // console.log(log.addGrade('25', 'Тригонометрия'));
 // console.log(log.getGrades());
 console.log(log);
