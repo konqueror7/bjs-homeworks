92993e60feff2bfc98966005bc7420d383810c13
konqueror7 <konqueror7@gmail.com>
2019-03-15 18:24:43 +0500

    oop/grades #11

--- oop-es6/main.js
+++ oop-es6/main.js
@@ -30,34 +30,6 @@ class StudentLog {
       this.gradesSubjects[subject].push(parseInt(grade));
       return this.gradesSubjects[subject].length;
     }
-    // const gradesSubject = this.search(subject);
-
-    // if (parseInt(grade) !== parseInt(grade) || parseInt(grade) > 5) {
-    //   console.log(`Вы пытались поставить оценку ${grade} по предмету ${subject}.\nМожно вводить только целое число от 1 до 5`);
-    // }
-    // else {
-    //   if (subject in this.gradesSubjects) {
-    //     this.gradesSubjects.[subject].push(parseInt(grade));
-    //     return this.gradesSubjects.[subject].length;
-    //   }
-    //
-    //   this.gradesSubjects.push({[subject]: parseInt(grade)});
-    // }
-    //
-    // return  this.gradesSubjects.[subject].length;
-  //   if (parseInt(grade) !== parseInt(grade) || parseInt(grade) > 5) {
-  //     console.log(`Вы пытались поставить оценку ${grade} по предмету ${subject}.\nМожно вводить только целое число от 1 до 5`);
-  //   }
-  //   else {
-  //     if (subject in this.gradesSubjects) {
-  //       this.gradesSubjects[subject].push(grade);
-  //     }
-  //     else {
-  //       this.gradesSubjects[subject] = [];
-  //       this.gradesSubjects[subject].push(grade);
-  //     }
-  //   }
-  //   return this.gradesSubjects[subject].length;
   }
 
   getAverageBySubject(subject) {
@@ -77,43 +49,47 @@ class StudentLog {
   getTotalAverage() {
     let totalAverage = 0;
     let totalCount = 0;
-    for (var subject in this.gradesSubjects) {
-      totalCount++;
+    for (let key in this.gradesSubjects) {
+      this.gradesSubjects[key].forEach(function(item) {
+        totalAverage += item;
+      });
+      totalCount += this.gradesSubjects[key].length;
+    }
+    if (totalAverage === 0) {
+      return 0;
     }
     return totalAverage / totalCount;
   }
 
   getGradesBySubject(subject) {
-    const gradesSubject = this.search(subject);
-    if (!gradesSubject) {
+    //const gradesSubject = this.search(subject);
+    if (!this.gradesSubjects[subject]) {
       return [];
     }
-    return console.log(gradesSubject.grades);
+    return this.gradesSubjects[subject];
   }
 
   getGrades() {
-
+    if (!this.gradesSubjects) {
+      return [];
+    }
+    return this.gradesSubjects;
   }
 
 }
 
 const log = new StudentLog('Василий Теркин');
 log.getName();
-// console.log(log);
-// log.addGrade(5, 'Геометрия');
-// log.addGrade(5, 'Геометрия');
-// console.log(log.addGrade(3, 'Тригонометрия'));
-// log.addGrade('5', 'География');
-// log.addGrade('3', 'География');
-// log.addGrade('4', 'Геометрия');
 console.log(log.addGrade('5', 'geography'));
 console.log(log.addGrade('5', 'geography'));
 console.log(log.addGrade('3', 'ariphmetic'));
 console.log(log.addGrade('4', 'ariphmetic'));
 console.log(log.addGrade('24', 'ariphmetic'));
-// console.log(log.addGrade('ОТлично!', 'Арифметика'));
+console.log(log.addGrade('ОТлично!', 'ariphmetic'));
+console.log(log.addGrade('25', 'geography'));
 console.log(log.getAverageBySubject('ariphmetic'));
 console.log(log.getTotalAverage());
-// console.log(log.addGrade('25', 'Тригонометрия'));
-// console.log(log.getGrades());
-console.log(log);
+console.log(log.getGradesBySubject('ariphmetic'));
+console.log(log.getGradesBySubject('math'));
+console.log(log.getGrades());
+// console.log(log);
